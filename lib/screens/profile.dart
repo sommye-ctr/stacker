4ce1f0739364e119e7fe92d2176e59974a62ef6c
Somye <mahajan.somye@gmail.com>
2024-10-20 03:45:42 +0530

    Configured profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 import 'package:stacker/screens/signup.dart';
+import 'package:stacker/screens/view_bookings.dart';
 import 'package:stacker/services/local.dart';
 import 'package:stacker/widgets/spacing.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -14,46 +15,62 @@ class ProfileScreen extends StatelessWidget {
     return Material(
       child: SafeArea(
         child: FScaffold(
-          content: Container(),
-          footer: FButton(
-              onPress: () async {
-                await Supabase.instance.client.auth.signOut();
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  SignupScreen.route,
-                  (route) => false,
-                );
-              },
-              label: const Text("Logout")),
+          footer: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FButton(
+                onPress: () async {
+                  await Supabase.instance.client.auth.signOut();
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    SignupScreen.route,
+                    (route) => false,
+                  );
+                },
+                label: const Text("Logout")),
+          ),
           header: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
+            child: Column(
               children: [
                 FAvatar(
                   image: const AssetImage(""),
                   fallback: Text(
-                    "",
+                    LocalService.getUsername().characters.first.toUpperCase(),
                     style: context.theme.typography.xl5,
                   ),
                   size: 100,
                 ),
                 const Spacing(),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        LocalService.getUsername(),
-                        style: context.theme.typography.xl3,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Text(LocalService.getUserEmail()),
-                  ],
+                Text(
+                  LocalService.getUsername(),
+                  style: context.theme.typography.xl3,
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  LocalService.getUserEmail(),
+                  style: context.theme.typography.sm,
+                ),
+                const Spacing(
+                  large: true,
                 ),
               ],
             ),
+          ),
+          content: Column(
+            children: [
+              FCard(
+                child: ListTile(
+                  title: const Text("View Bookings"),
+                  enabled: true,
+                  trailing: FAssets.icons.chevronRight(),
+                  leading: FAssets.icons.libraryBig(),
+                  onTap: () =>
+                      Navigator.pushNamed(context, ViewBookingsScreen.route),
+                ),
+              ),
+            ],
           ),
         ),
       ),
